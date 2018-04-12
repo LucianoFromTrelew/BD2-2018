95dc89f040d40f83dc0cbc257ed4aa5efcf137f9
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-04-12 11:38:50 -0300

    Agregar correciones de Cristian

--- tp1/src/select_reparaciones.sql
+++ tp1/src/select_reparaciones.sql
@@ -1,12 +1,12 @@
 /*
     Consulta sobre aviones que tengan una capacidad de 1959
  */
-SELECT  tr."dniTrabajador", 
-        tr."nroAvion",
-        tr."fechaInicioReparacion",
-        tr."fechaFinReparacion",
-        md.descripcion,
-        f.descripcion
+SELECT  tr."dniTrabajador" AS "DNI", 
+        tr."nroAvion" as "Avión",
+        tr."fechaInicioReparacion" AS "Fecha de comienzo",
+        tr."fechaFinReparacion" AS "Fecha de fin",
+        md.descripcion AS "Modelo",
+        f.descripcion AS "Falla reparada"
         FROM 
             "trabajadorReparacion" AS tr,
             "modeloAvion" AS md,
@@ -15,4 +15,5 @@ SELECT  tr."dniTrabajador",
         WHERE
             tr."nroAvion" = av."nroAvion"
             AND av."tipoModelo" = md."tipoModelo"
-            AND md.capacidad = 1959;
+            AND md.capacidad = 1959
+            AND f."tipoFalla" = tr."tipoFallaReparada";
