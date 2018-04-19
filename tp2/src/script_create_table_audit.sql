2a6cd320a0e6e75450a33a9320df042ef9a201f9
LucianoFromTrelew <lucianoserruya@gmail.com>
2018-04-19 00:23:50 -0300

    Agregar scripts para la tabla 'audit'
    Scripts para crear la tabla 'audit' con los constraints necesarios

--- tp2/src/script_create_table_audit.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE IF NOT EXISTS audit (
-    tabla TEXT,
-    operacion TIPO_OPERACION,
-    usuario TEXT,
-    fecha_hora TIMESTAMP
-);
---
+++ tp2/src/script_tabla_audit.sql
@@ -0,0 +1,30 @@
+﻿-- Tipo de dato enumerado para las operacioes
+CREATE TYPE TIPO_OPERACION AS ENUM ('SELECT', 'INSERT', 'UPDATE', 'DELETE');
+
+-- Tabla para auditoría
+CREATE TABLE IF NOT EXISTS audit (
+    tabla TEXT,
+    operacion TIPO_OPERACION,
+    usuario TEXT,
+    fecha_hora TIMESTAMP PRIMARY KEY,
+    -- Regla para verificar que el nombre de la tabla sea correcto
+    CONSTRAINT "check_nombre_tabla" CHECK(
+        es_nombre_tabla(tabla)
+    )
+);
+
+CREATE OR REPLACE FUNCTION es_nombre_tabla(tabla text) RETURNS BOOLEAN AS 
+-- Verifica si el nombre a ingresar en el campo 'tabla' se corresponde con una tabla de la BD
+-- Recibe un nombre de tabla
+-- Devuelve un valor lógico
+$$
+BEGIN
+    RETURN (SELECT tabla IN (
+            SELECT table_name 
+            FROM information_schema.tables 
+            WHERE table_schema='public'
+    ));
+END;
+$$ LANGUAGE plpgsql;
+
+
