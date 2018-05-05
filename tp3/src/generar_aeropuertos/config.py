fd0d5130b9c6489245c8d790841f3cfe3d0b7825
Luciano <lucianoserruya@gmail.com>
2018-05-05 18:04:21 -0300

    Agregar script de carga de aeropuertos
    Agregué el script .sql para cargar los aeropuertos, y también el script
    .py para generar el script para cargar los aeropuertos

---
+++ tp3/src/generar_aeropuertos/config.py
@@ -0,0 +1,9 @@
+# Códigos de los materiales sacados de:
+# https://en.wikipedia.org/wiki/Runway
+COMPUESTOS = ['ASP', 'BIT', 'BRI', 'CLA', 'COM', 'CON', 'COP', 'COR', 'GRE', 'GVL', 'GRS', 'ICE', 'LAT', 'MAC', 'PEM', 'PER', 'PSP', 'SAN', 'SMT', 'SNO', 'U']
+MIN_LONG        = 1000
+MAX_LONG        = 5000
+MIN_ANCHO       = 10
+MAX_ANCHO       = 500
+MIN_AEROLINEAS  = 10
+MAX_AEROLINEAS  = 150
---
+++ tp3/src/generar_aeropuertos/generar_aeropuertos.py
@@ -0,0 +1,65 @@
+import csv
+import random
+import config
+
+def get_random_int(floor, roof):
+    """Devuelve un entero aleatorio entre 'floor' y 'roof'"""
+    return random.randint(floor, roof)
+
+def get_random_choices(population, k):
+    """Devuelve una lista de k-elementos aleatorios de 'population'"""
+    return random.sample(population, k)
+
+def get_data_from_csv(file):
+    """Devuelve un csv 'file' entero en una lista"""
+    data = []
+    with open(file, newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            data.append(row)
+
+    return data
+
+def get_airport_insert_string(aeropuerto):
+    """Devuelve una cadena formateada para insertar un aeropuerto"""
+    return "INSERT INTO aeropuerto VALUES('{nombre}', ROW('{ciudad}', '{ciudad}', '{pais}'), ROW({longitud}, {ancho}, '{compuesto}'), ARRAY{aerolineas});\n".format(
+        nombre=aeropuerto['nombre'],
+        ciudad=aeropuerto['ciudad'],
+        pais=aeropuerto['pais'],
+        longitud=aeropuerto['longitud'],
+        ancho=aeropuerto['ancho'],
+        compuesto=aeropuerto['compuesto'],
+        aerolineas=aeropuerto['aerolineas']
+    )
+
+def main():
+    with open("script_carga_aeropuertos.sql", "w") as f:
+        with open("data/airports.csv", newline='') as airports_csv:
+            reader = csv.DictReader(airports_csv)
+            airlines = get_data_from_csv('data/airlines.csv')
+            for airport in reader:
+                aeropuerto = {}
+                aeropuerto['nombre'] = airport['name'].replace("\'", " ")
+                aeropuerto['ciudad'] = airport['city'].replace("\'", " ")
+                aeropuerto['pais'] = airport['country'].replace("\'", " ")
+                aeropuerto['longitud'] = get_random_int(
+                    config.MIN_LONG, config.MAX_LONG)
+
+                aeropuerto['ancho'] = get_random_int(
+                    config.MIN_ANCHO, config.MAX_ANCHO)
+
+                aeropuerto['compuesto'] = get_random_choices(
+                    config.COMPUESTOS, 1)[0]
+
+                aeropuerto['aerolineas'] = [
+                    '{}'.format(airline['name'].replace("\'", " ")) for airline in get_random_choices(
+                        airlines, get_random_int(
+                            config.MIN_AEROLINEAS,
+                            config.MAX_AEROLINEAS))
+                ]
+
+                f.write(get_airport_insert_string(aeropuerto))
+
+
+if __name__ == "__main__":
+    main()
