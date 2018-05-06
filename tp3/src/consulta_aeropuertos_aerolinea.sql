2818f5e04d6a261d253a35972831601d31bc7678
Luciano <lucianoserruya@gmail.com>
2018-05-05 21:28:19 -0300

    Agregar consulta de aeropuertos que trabajan con aerolinea

---
+++ tp3/src/consulta_aeropuertos_aerolinea.sql
@@ -0,0 +1,11 @@
+SELECT nombre, (ubicacion).ciudad 
+    FROM aeropuerto 
+    WHERE 'Aerolineas Argentinas' = ANY(aerolineas);
+
+SELECT nombre, (ubicacion).ciudad AS ciudad, nombres_aerolineas 
+    FROM aeropuerto, UNNEST(aerolineas) AS nombres_aerolineas 
+    WHERE nombres_aerolineas ILIKE '%argentina%' ORDER BY nombres_aerolineas, ciudad;
+
+-- select nombre, ciudad from (
+    -- select nombre, (ubicacion).ciudad as ciudad, unnest(aerolineas) as nombre_aerolinea from aeropuerto) as sq 
+-- where nombre_aerolinea ilike '%argentina%'
