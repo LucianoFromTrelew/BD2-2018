9371ebc5e23a9a9d80a7635286178a67be575b08
Luciano <lucianoserruya@gmail.com>
2018-05-28 17:28:31 -0300

    Agregar consultas pilotos de Trelew
    Las consultas correspondientes para armar el fragmento con los pilotos
    de Trelew

--- tp4/src/pilotos_de_trelew.sql
+++ tp4/src/pilotos_de_trelew.sql
@@ -1,21 +1,78 @@
--- SELECT * 
--- FROM piloto 
--- WHERE "dniPiloto" IN (
-    -- SELECT dni 
-    -- FROM TRABAJADOR AS t 
-    -- WHERE t."idLocalidad" = (
-        -- SELECT "idLocalidad" 
-        -- FROM localidad 
-        -- WHERE "nombreLocalidad" ILIKE '%trelew%'
-    -- )
--- );
+-- Localidad de Trelew
+SELECT *
+FROM localidad
+WHERE "nombreLocalidad"
+    ILIKE '%trelew%';
 
+-- Pilotos de Trelew
 SELECT * 
-FROM piloto AS p
-JOIN trabajador AS t
-ON t.dni = p."dniPiloto"
+FROM piloto 
+WHERE "dniPiloto" IN (
+    SELECT dni 
+    FROM TRABAJADOR AS t 
+    WHERE t."idLocalidad" = (
+        SELECT "idLocalidad" 
+        FROM localidad 
+        WHERE "nombreLocalidad" ILIKE '%trelew%'
+    )
+);
+
+-- Trabajadores de Trelew
+SELECT * 
+FROM trabajador AS t
 WHERE t."idLocalidad" = (
     SELECT "idLocalidad" 
     FROM localidad 
     WHERE "nombreLocalidad" ILIKE '%trelew%'
+);
+
+-- Relación entre piloto y avión de los pilotos de Trelew
+SELECT *
+FROM "pilotoAvion" AS pa
+WHERE pa."dniPiloto" IN (
+    SELECT dni 
+    FROM trabajador AS t
+    WHERE t."idLocalidad" = (
+        SELECT "idLocalidad" 
+        FROM localidad 
+        WHERE "nombreLocalidad" ILIKE '%trelew%'
+    )
+);
+
+-- Aviones piloteados por pilotos de Trelew
+SELECT *
+FROM avion
+WHERE "nroAvion" in (
+    SELECT "nroAvion"
+    FROM "pilotoAvion" AS pa
+    WHERE pa."dniPiloto" IN (
+        SELECT dni 
+        FROM trabajador AS t
+        WHERE t."idLocalidad" = (
+            SELECT "idLocalidad" 
+            FROM localidad 
+            WHERE "nombreLocalidad" ILIKE '%trelew%'
+        )
+    )
+);
+
+-- Modelos de aviones piloteados por pilotos de Trelew
+SELECT *
+FROM "modeloAvion"
+WHERE "tipoModelo" IN (
+    SELECT "tipoModelo"
+    FROM avion
+    WHERE "nroAvion" in (
+        SELECT "nroAvion"
+        FROM "pilotoAvion" AS pa
+        WHERE pa."dniPiloto" IN (
+            SELECT dni 
+            FROM trabajador AS t
+            WHERE t."idLocalidad" = (
+                SELECT "idLocalidad" 
+                FROM localidad 
+                WHERE "nombreLocalidad" ILIKE '%trelew%'
+            )
+        )
+    )
 );
