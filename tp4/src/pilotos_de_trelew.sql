635f9bc1a6777eb65087776be0b2aa14acd18e57
Luciano <lucianoserruya@gmail.com>
2018-05-28 02:55:50 -0300

    Agregar avances

--- tp4/src/pilotos_de_trelew.sql
+++ tp4/src/pilotos_de_trelew.sql
@@ -1,11 +1,21 @@
+-- SELECT * 
+-- FROM piloto 
+-- WHERE "dniPiloto" IN (
+    -- SELECT dni 
+    -- FROM TRABAJADOR AS t 
+    -- WHERE t."idLocalidad" = (
+        -- SELECT "idLocalidad" 
+        -- FROM localidad 
+        -- WHERE "nombreLocalidad" ILIKE '%trelew%'
+    -- )
+-- );
+
 SELECT * 
-FROM piloto 
-WHERE "dniPiloto" IN (
-    SELECT dni 
-    FROM TRABAJADOR AS t 
-    WHERE t."idLocalidad" = (
-        SELECT "idLocalidad" 
-        FROM localidad 
-        WHERE "nombreLocalidad" ILIKE '%trelew%'
-    )
+FROM piloto AS p
+JOIN trabajador AS t
+ON t.dni = p."dniPiloto"
+WHERE t."idLocalidad" = (
+    SELECT "idLocalidad" 
+    FROM localidad 
+    WHERE "nombreLocalidad" ILIKE '%trelew%'
 );
