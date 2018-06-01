ee7525382b452b53b238eea3b9ad04cceec02dcf
Luciano <lucianoserruya@gmail.com>
2018-06-01 16:13:02 -0300

    Agregar consulta todos los pilotos
    Esquema de fragmentación de todos los pilotos

---
+++ tp4/src/todos_pilotos.sql
@@ -0,0 +1,21 @@
+/*
+    Los siguientes fragmentos serían asignados al sitio que tiene todos los pilotos
+ */
+
+-- Todas las localidades 
+SELECT * FROM localidad;
+
+-- Todos los pilotos
+SELECT * FROM piloto;
+
+-- Todos los trabajadores
+SELECT * FROM trabajador;
+
+-- Relación entre piloto y avión de todos los pilotos
+SELECT * FROM "pilotoAvion"; 
+
+-- Todos los aviones
+SELECT * FROM avion;
+
+-- Todos los modelos de avión
+SELECT * FROM "modeloAvion";
